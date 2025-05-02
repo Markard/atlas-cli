a9d756d7badcd1170c4b445212a442ed280f0a73
Dmitrii Iuzhakov <dmtry88@gmail.com>
2025-05-02 21:48:51 +0300

    Setup base configuration, logger

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"atlas-cli/pkg/logger"
+	"fmt"
+	"github.com/joho/godotenv"
+	"io"
+	"log/slog"
+	"os"
+)
+
+func main() {
+	errDotEnvLoad := godotenv.Load()
+	if errDotEnvLoad != nil {
+		fmt.Println(errDotEnvLoad)
+		os.Exit(1)
+	}
+
+	logFile, err := os.OpenFile(os.Getenv("LOG_FILE"), os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		panic(err)
+	}
+	defer logFile.Close()
+	slog.SetDefault(setupPrettySlog(logFile))
+}
+
+func setupPrettySlog(logFile *os.File) *slog.Logger {
+	opts := logger.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelInfo,
+		},
+	}
+
+	mw := io.MultiWriter(os.Stdout, logFile)
+	handler := opts.NewPrettyHandler(mw)
+
+	return slog.New(handler)
+}
